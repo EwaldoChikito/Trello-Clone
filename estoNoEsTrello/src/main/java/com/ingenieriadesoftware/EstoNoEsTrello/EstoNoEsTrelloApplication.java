64e713693c92ea23c32d2792b859382fc71652ca
Alberto Rodríguez Villanueva <135548602+Albert-PE@users.noreply.github.com>
2025-04-14 19:59:47 -0400

    add good shyt

---
+++ estoNoEsTrello/src/main/java/com/ingenieriadesoftware/EstoNoEsTrello/EstoNoEsTrelloApplication.java
@@ -0,0 +1,13 @@
+package com.ingenieriadesoftware.EstoNoEsTrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoNoEsTrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoNoEsTrelloApplication.class, args);
+	}
+
+}
---
+++ estoNoEsTrello/src/test/java/com/ingenieriadesoftware/EstoNoEsTrello/EstoNoEsTrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenieriadesoftware.EstoNoEsTrello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoNoEsTrelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
