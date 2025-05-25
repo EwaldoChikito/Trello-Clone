10ea25b9efcbda13e5faa46fa0048fb2acc14780
sdartiles24 <sdartiles.24@est.ucab.edu.ve>
2025-05-25 16:26:12 -0400

    Constructor y atributo password a User

--- estoNoEsTrello/src/main/java/com/ingenieriadesoftware/EstoNoEsTrello/model/User.java
+++ estoNoEsTrello/src/main/java/com/ingenieriadesoftware/EstoNoEsTrello/model/User.java
@@ -5,7 +5,22 @@ import java.util.Arrays;
 public class User {
     private String username;
     private String email;
+    private String password;
     private WorkSpace[] workspaces;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getUsername() {
         return username;
