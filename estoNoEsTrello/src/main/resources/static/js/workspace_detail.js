3e9e17aea9add138b3df3e699e1adbc985c29075
Ewaldo <iguachente@gmail.com>
2025-07-10 01:19:48 -0400

    Fix WorkspaceDetail Buttons

--- estoNoEsTrello/src/main/resources/static/js/workspace_detail.js
+++ estoNoEsTrello/src/main/resources/static/js/workspace_detail.js
@@ -87,14 +87,11 @@ function renderBoard(blocks) {
 
             // Edit button
             const editBtn = document.createElement('button');
-            editBtn.textContent = "✎";
+            editBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+            </svg>`;
             editBtn.title = "Edit block name";
-            editBtn.className = 'edit-block-title-btn';
-            editBtn.style.marginLeft = "8px";
-            editBtn.style.background = "none";
-            editBtn.style.border = "none";
-            editBtn.style.cursor = "pointer";
-            editBtn.style.fontSize = "1.1em";
+            editBtn.className = 'edit-block-title-btn card1-icon';
             editBtn.onclick = function(e) {
                 e.stopPropagation();
                 // Replace title with input
@@ -102,11 +99,6 @@ function renderBoard(blocks) {
                 input.type = "text";
                 input.value = list.title || list.name || "";
                 input.className = "edit-block-title-input";
-                input.style.flex = "1";
-                input.style.fontSize = "1em";
-                input.style.padding = "2px 6px";
-                input.style.borderRadius = "6px";
-                input.style.border = "1px solid #ccc";
                 listTitleBar.replaceChild(input, listTitle);
                 input.focus();
                 input.select();
@@ -125,8 +117,25 @@ function renderBoard(blocks) {
                 }
             };
 
+            // Delete button
+            const deleteBtn = document.createElement('button');
+            deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+            </svg>`;
+            deleteBtn.title = "Delete block";
+            deleteBtn.className = 'delete-block-title-btn card1-icon';
+            deleteBtn.onclick = function(e) {
+                e.stopPropagation();
+                if (confirm('¿Estás seguro de que quieres eliminar este bloque?')) {
+                    blocks.splice(blockIdx, 1);
+                    renderBoard(blocks);
+                }
+            };
+
+
             listTitleBar.appendChild(listTitle);
             listTitleBar.appendChild(editBtn);
+            listTitleBar.appendChild(deleteBtn);
             listDiv.appendChild(listTitleBar);
 
             // --- Cards ---
@@ -174,6 +183,22 @@ function renderBoard(blocks) {
                         e.stopPropagation();
                         moveCard(blockIdx, cardIdx, 1);
                     };
+
+                    // Delete card button
+                    const deleteCardBtn = document.createElement('button');
+                    deleteCardBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>`;
+                    deleteCardBtn.title = "Delete card";
+                    deleteCardBtn.className = 'delete-card-btn';
+                    deleteCardBtn.onclick = function(e) {
+                        e.stopPropagation();
+                        if (confirm('¿Estás seguro de que quieres eliminar esta tarjeta?')) {
+                            blocks[blockIdx].cards.splice(cardIdx, 1);
+                            renderBoard(blocks);
+                        }
+                    };
+                    arrows.appendChild(deleteCardBtn);
 
                     arrows.appendChild(upBtn);
                     arrows.appendChild(downBtn);
