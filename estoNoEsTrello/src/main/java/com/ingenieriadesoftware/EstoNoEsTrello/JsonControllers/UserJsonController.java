2a1b6b16b73b7cca788dece1815cd073576eb006
Ewaldo <iguachente@gmail.com>
2025-07-10 00:52:18 -0400

    Fix Edit & Delete Buttons

--- estoNoEsTrello/src/main/java/com/ingenieriadesoftware/EstoNoEsTrello/JsonControllers/UserJsonController.java
+++ estoNoEsTrello/src/main/java/com/ingenieriadesoftware/EstoNoEsTrello/JsonControllers/UserJsonController.java
@@ -50,14 +50,14 @@ public class UserJsonController extends User {
     static public void saveUser(User user){
         try {
             Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
-            JsonReader reader = new JsonReader(new FileReader("C://Users//2samd//Proyectos//Proyecto Software//estoNoEsTrello//src//main//resources//JSONs//Users.json"));
+            JsonReader reader = new JsonReader(new FileReader("C:\\Users\\iguac\\Documents\\CODE\\Inge SW\\Trello-Clone\\estoNoEsTrello\\src\\main\\resources\\JSONs\\Users.json"));
 //            JsonReader reader = new JsonReader(new FileReader(USERS_JSON_PATH));
             User[] users = gson.fromJson(reader, User[].class);
             List<User> userList= new ArrayList<>(Arrays.asList(users));
 
             userList.add(user);
 
-            FileWriter fw = new FileWriter("C://Users//2samd//Proyectos//Proyecto Software//estoNoEsTrello//src//main//resources//JSONs//Users.json");
+            FileWriter fw = new FileWriter("C:\\Users\\iguac\\Documents\\CODE\\Inge SW\\Trello-Clone\\estoNoEsTrello\\src\\main\\resources\\JSONs\\Users.json");
 //            FileWriter fw = new FileWriter(USERS_JSON_PATH);
             StringWriter sw = new StringWriter();
             sw.write(gson.toJson(userList));
@@ -72,7 +72,7 @@ public class UserJsonController extends User {
     static public ArrayList<User> findTotalUsers(){
         try {
             Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
-            JsonReader reader = new JsonReader(new FileReader("C://Users//2samd//Proyectos//Proyecto Software//estoNoEsTrello//src//main//resources//JSONs//Users.json"));
+            JsonReader reader = new JsonReader(new FileReader("C:\\Users\\iguac\\Documents\\CODE\\Inge SW\\Trello-Clone\\estoNoEsTrello\\src\\main\\resources\\JSONs\\Users.json"));
 //            JsonReader reader = new JsonReader(new FileReader(USERS_JSON_PATH));
             User[] users = gson.fromJson(reader, User[].class);
             return new ArrayList<>(Arrays.asList(users));
@@ -83,7 +83,7 @@ public class UserJsonController extends User {
 
     static public void deleteUser(String email) throws IOException{
         Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
-        List<User> users = gson.fromJson(new FileReader("C://Users//2samd//Proyectos//Proyecto Software//estoNoEsTrello//src//main//resources//JSONs//Users.json"), new TypeToken<List<User>>() {}.getType());
+        List<User> users = gson.fromJson(new FileReader("C:\\Users\\iguac\\Documents\\CODE\\Inge SW\\Trello-Clone\\estoNoEsTrello\\src\\main\\resources\\JSONs\\Users.json"), new TypeToken<List<User>>() {}.getType());
 //        List<User> users = gson.fromJson(new FileReader(USERS_JSON_PATH), new TypeToken<List<User>>() {}.getType());
         // Eliminar el producto
         List<User> updatedClients = new ArrayList<>();
@@ -94,7 +94,7 @@ public class UserJsonController extends User {
         }
 
         // Escribir el JSON actualizado
-        try (FileWriter writer = new FileWriter("C://Users//2samd//Proyectos//Proyecto Software//estoNoEsTrello//src//main//resources//JSONs//Users.json")) {
+        try (FileWriter writer = new FileWriter("C:\\Users\\iguac\\Documents\\CODE\\Inge SW\\Trello-Clone\\estoNoEsTrello\\src\\main\\resources\\JSONs\\Users.json")) {
 //        try (FileWriter writer = new FileWriter(USERS_JSON_PATH)) {
             gson.toJson(updatedClients, writer);
         }
